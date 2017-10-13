d1b743c9c6e39d18105d3d58714bfff6570adff7
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-10-13 12:52:19 -0400

    slide errorr

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ include 'header.php';
          <h1> This Month's Featured Items </h1>
       </div>
       <div class="banner">
-      <img src="lib/img/img/circuit.jpg" alt="The Circuit Book" ><a href="detailpage.php?id=3"></a>
-      <img src="lib/img/img/popo.jpg" alt="Popocatepec" ><a href="detailpage.php?id=9"></a>
-      <img src="lib/img/img/dog.jpeg" alt="Dog Shirt" ><a href="detailpage.php?id=16"></a>
-      <img src="lib/img/img/sor-juana.jpg" alt="sor-juana" ><a href="detailpage.php?id=12"></a>
+      <img onclick="window.location='detailpage.php?id=3'" src="lib/img/img/circuit.jpg" alt="The Circuit Book" >
+      <img onclick="window.location='detailpage.php?id=9'"  src="lib/img/img/popo.jpg" alt="Popocatepec" >
+      <img onclick="window.location='detailpage.php?id=16'" src="lib/img/img/dog.jpeg" alt="Dog Shirt" >
+      <img onclick="window.location='detailpage.php?id=12'" src="lib/img/img/sor-juana.jpg" alt="sor-juana" >
       </div>
    </main>
    <div class="bottomIndex">
