49e3f1c63da97d1ecd2dd4612a08831e6faad40c
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-10-13 15:10:18 -0400

    errorr

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ include 'header.php';
          <h1> This Month's Featured Items </h1>
       </div>
       <div class="banner">
-      <img onclick="window.location='detailpage.php?id=3'" src="https://images.gr-assets.com/books/1348958233l/1369609.jpg" alt="The Circuit Book" >
-      <img onclick="window.location='detailpage.php?id=9'"  src="http://orig07.deviantart.net/406e/f/2013/224/4/5/45a2adb42db30d23ecf68a17e31c0f42-d6hrsg3.jpg" alt="Popocatepec" >
-      <img onclick="window.location='detailpage.php?id=16'" src="https://ae01.alicdn.com/kf/HTB1FeYsRXXXXXX0apXXq6xXFXXXm/New-font-b-T-b-font-font-b-Shirts-b-font-Unisex-Funny-Tops-Tee-Basic.jpg" alt="Dog Shirt" >
-      <img onclick="window.location='detailpage.php?id=12'" src="http://caleb.mx/tuvoz/wp-content/uploads/2013/04/sor-juana-3.jpg" alt="sor-juana" >
+      <img onclick="window.location='detailpage.php?id=3'" src="lib/img/img/circuit.jpg" alt="The Circuit Book" >
+      <img onclick="window.location='detailpage.php?id=9'"  src="lib/img/img/popo.jpg" alt="Popocatepec" >
+      <img onclick="window.location='detailpage.php?id=16'" src="lib/img/img/dog.jpeg" alt="Dog Shirt" >
+      <img onclick="window.location='detailpage.php?id=12'" src="lib/img/img/sor-juana.jpg" alt="sor-juana" >
       </div>
    </main>
    <div class="bottomIndex">
