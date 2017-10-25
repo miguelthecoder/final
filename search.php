09137375b009976be50acfcb3a124b4b02fe1de8
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-10-25 00:40:11 -0400

    fixed content

--- search.php
+++ search.php
@@ -3,9 +3,9 @@ $title="Search Page";
 include 'header.php';
 $searchTerm =!empty($_GET['searchTerm']) ? $_GET['searchTerm'] : '';
 ?>
-    <div class="produContain2">
     <h1>Welcome to the search Page</h1>
     <div class="search">
+    <div class="produContain2">
       <?php
         try{
         // connect to the DB
